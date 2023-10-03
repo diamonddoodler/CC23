13317f0f50ef81d5a983bc22b15d07502888b664
diamonddoodler <47303368+diamonddoodler@users.noreply.github.com>
2023-10-02 23:28:31 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,32 +1,67 @@
-let x, y
-let xDir, yDir
-let r = 25
+let myTri
+let myTri2
+let myTri3
+//let numTri = random(5, 15)
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width/2
-  y = height/2
+  
+  myTri = new Tri()
+  myTri2 = new Tri()
+  myTri3 = new Tri()
 
-  xDir = random(3, 8)
-  yDir = random(3, 8)
+  /*
+  for (let i = 0; i < numTri; i++) {
+    myTri[i] = new Tri()
+  }
+  */
 }
 
 function draw() {
-  //background(0);
-  r = random(20, 60)
-  ellipse(x, y, r*2)
+  background(0);
 
-  if (x>=windowWidth-r || x<= r) {
-    xDir *= -1
-    fill(random(255), random(255), random(255))
-    background(random(0, 225), random(0, 255), random(0, 255))
+  /*
+  for (let i = 0; i < numTri; i++) {
+    myTri[i].render()
+    myTri[i].strobe()
   }
-  if (y>=windowHeight-r || y<=r) {
-    yDir *= -1
-    fill(random(255), random(255), random(255))
-    background(random(0, 225), random(0, 255), random(0, 255))
+  */
+
+  myTri.render()
+  myTri.strobe()
+
+  myTri2.render()
+  myTri2.strobe()
+
+  myTri3.render()
+  myTri3.strobe()
+}
+
+class Tri {
+  constructor() {
+    this.col = color(random(255), random(255), random(255))
+    this.x = random(width)
+    this.y = random(height)
+    this.x2 = random(width)
+    this.y2 = random(height)
+    this.x3 = random(width)
+    this.y3 = random(height)
+    
+    this.xdir = random(1, 15)
+    this.ydir = random(1, 15)
+    this.size = random(30, 300)
+    this.stroke = random(1, 10)
   }
 
-  x = x+xDir
-  y = y+yDir
+  render() {
+    noFill()
+    stroke(this.col)
+    strokeWeight(this.stroke)
+    triangle(this.x, this.y, this.x2, this.y2, this.x3, this.y3)
+  }
+
+  strobe() {
+    this.x += this.xdir
+    this.y += this.ydir
+  }
 }
