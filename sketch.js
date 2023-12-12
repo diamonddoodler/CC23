fee71e9c340485494b8bfae33e0029ad1d14df8a
diamonddoodler <47303368+diamonddoodler@users.noreply.github.com>
2023-12-12 00:14:38 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,54 +1,73 @@
+var noiseScale = 100;
+var n = 500;
+let particles = []
+let particleColor;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  confet = new Confetti()
-  confet2 = new Confetti()
-  confet3 = new Confetti()
-  confet4 = new Confetti()
-  confet5 = new Confetti()
-  confet6 = new Confetti()
-  confet7 = new Confetti()
+  noiseDetail(1, 0)
+  genNoiseImg();
+  image(noiseImg, 0, 0);
+  
+  // making le particles with different colors
+  for (var i = 0; i < n; i++) {
+    var particle = new Object();
+    particle.pos = createVector(random(width), random(height));
+    
+    particle.color = color(random(255), random(255), random(255));
+    
+    particles.push(particle); // Adding le particle to le particle list
+  }
 }
 
-class Confetti {
-  constructor() {
-    this.col = color(random(255), random(255), random(255))
-    this.pos = createVector(random(0, width))
-    this.vel = createVector(random(-5, 5), random(1, 10))
-    this.stroke = random(6, 30)
-    this.size = random(30, 100)
-  }
+function curl(x, y) {
+  var EPSILON = 0.001;
   
-  render() {
-    stroke(this.col)
-    strokeWeight(this.stroke)
-    point(this.pos)
-  }
+  //rate of change in x-direction
+  var n1 = noise(x + EPSILON, y);
+  var n2 = noise(x - EPSILON, y);
+  var curlX = (n1 - n2)/(2 * EPSILON)
   
-  update() {
-    this.pos.add(this.vel)
-  }
+  //rate of change in y-direction
+  n1 = noise(x, y + EPSILON);
+  n2 = noise(x, y - EPSILON);
+  
+  var curlY = (n1 - n2)/(2 * EPSILON);
+  
+  //return new createVector(curlX, curlY);
+  return new createVector(curlY, -curlX) //rotates 90 degrees
 }
 
 function draw() {
-  background(0);
-  confet.render()
-  confet.update()
+  tint(255, 10);
+  image(noiseImg, 0, 0);
 
-  confet2.render()
-  confet2.update()
+  for (var i = 0; i < particles.length; i++) {
+    var p = particles[i];
+    stroke(p.color); //assigned colors for each particle
+    strokeWeight(random(2, 6)); // Particle sizes
+    
+    p.pos.add(curl(p.pos.x / noiseScale, p.pos.y / noiseScale));
+    point(p.pos.x, p.pos.y);
+  }
+}
 
-  confet3.render()
-  confet3.update()
+function genNoiseImg() {
+  noiseImg = createGraphics(width, height);
+  noiseImg.loadPixels();
+  var wDensity = width * pixelDensity();
+  var hDensity = height * pixelDensity();
   
-  confet4.render()
-  confet4.update()
-  
-  confet5.render()
-  confet5.update()
-  
-  confet6.render()
-  confet6.update()
-  
-  confet7.render()
-  confet7.update()
+  for (var i = 0; i < wDensity; i++) {
+    for(var j = 0; j < hDensity; j++) {
+      var x = i/pixelDensity();
+      var y = j/pixelDensity();
+      var bright = pow(noise(x / noiseScale, y / noiseScale) - 0.3, 1/2.0) * 500;
+      noiseImg.pixels[(i+j*wDensity) * 4] = bright;
+      noiseImg.pixels[(i+j*wDensity) * 4 + 1] = bright;
+      noiseImg.pixels[(i+j*wDensity) * 4 + 2] = bright;
+      noiseImg.pixels[(i+j*wDensity) * 4 + 3] = 255;
+    }
+  }
+  noiseImg.updatePixels()
 }
